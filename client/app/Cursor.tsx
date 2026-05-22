8b55772115510f7d450cd16a8869afb57ae7aa37
vgnz93hs <ilce6400.mail@icloud.com>
2026-05-22 21:41:38 +0900

    feature: install framer-motion

--- client/app/Cursor.tsx
+++ client/app/Cursor.tsx
@@ -1,9 +1,27 @@
-"use client"
+"use client";
 
-import { useState, useEffect, useRef } from "react"
+import { useState, useEffect, useRef } from "react";
 
 export function Cursor() {
-    const [position, setPosition] = useState<{ x: number; y: number }>({ x: 0, y: 0 })
+    const [tergetPosition, setTergetPosition] = useState<{
+        x: number;
+        y: number;
+    }>({
+        x: 0,
+        y: 0,
+    });
+    const [tergetSize, setTergetSize] = useState<{ x: number; y: number }>({
+        x: 25,
+        y: 25,
+    });
+    const [position, setPosition] = useState<{ x: number; y: number }>({
+        x: 0,
+        y: 0,
+    });
+    const [size, setSize] = useState<{ x: number; y: number }>({
+        x: 25,
+        y: 25,
+    });
 
     useEffect(() => {
         const handleMouseMove = (e: MouseEvent) => {
@@ -14,8 +32,20 @@ export function Cursor() {
 
         return () => {
             window.removeEventListener("mousemove", handleMouseMove);
-        }
+        };
     }, []);
 
-    return <div className="fixed w-4 h-4 rounded-full border border-white pointer-events-none" style={{ left: `${position.x}px`, top: `${position.y}px` }} />;
+    return (
+        <div
+            className="fixed rounded-full pointer-events-none"
+            style={{
+                left: `${position.x - size.x / 2}px`,
+                top: `${position.y - size.y / 2}px`,
+                width: `${size.x}px`,
+                height: `${size.y}px`,
+            }}
+        >
+            <div className="w-full h-full bg-white rounded-full" />
+        </div>
+    );
 }
