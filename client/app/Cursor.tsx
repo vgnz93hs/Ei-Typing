0526227c341a067f2f04d7fb95f4858943c5bf6c
vgnz93hs <ilce6400.mail@icloud.com>
2026-05-22 22:32:54 +0900

    feature: add cursor movement program

--- client/app/Cursor.tsx
+++ client/app/Cursor.tsx
@@ -1,22 +1,30 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { motion, useSpring, useTransform } from "framer-motion";
+import { motion, useSpring } from "framer-motion";
 
 export function Cursor() {
-    const [target, setTarget] = useState({ x: 0, y: 0, width: 25, height: 25 });
+    const [target, setTarget] = useState({
+        x: 0,
+        y: 0,
+        width: 25,
+        height: 25,
+        weight: 0,
+    });
 
-    const springConfig = { stiffness: 150, damping: 20, mass: 1 };
+    const springConfig = { stiffness: 1000, damping: 50, mass: 1 };
     const cursorX = useSpring(0, springConfig);
     const cursorY = useSpring(0, springConfig);
     const cursorW = useSpring(25, springConfig);
     const cursorH = useSpring(25, springConfig);
+    const cursorWeight = useSpring(25, springConfig);
 
     useEffect(() => {
         cursorX.set(target.x - target.width / 2);
         cursorY.set(target.y - target.height / 2);
         cursorW.set(target.width);
         cursorH.set(target.height);
+        cursorWeight.set(target.weight);
     }, [target, cursorX, cursorY, cursorW, cursorH]);
 
     useEffect(() => {
@@ -26,6 +34,7 @@ export function Cursor() {
                 y: e.clientY,
                 width: 25,
                 height: 25,
+                weight: 0,
             });
         };
 
