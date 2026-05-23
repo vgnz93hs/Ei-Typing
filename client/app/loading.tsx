b26f83522876d3d6963130d6513f29bd62a99558
vgnz93hs <142011868+vgnz93hs@users.noreply.github.com>
2026-05-23 12:20:25 +0900

    feature: add loading page (#6)

--- client/app/loading.tsx
+++ client/app/loading.tsx
@@ -1,7 +1,27 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
 export default function Loading() {
+    const [showCursor, setShowCursor] = useState(true);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setShowCursor((prev) => !prev);
+        }, 500);
+    }, []);
+
     return (
-        <div className="flex justify-center items-center h-full">
-            <p>ページを読み込み中...</p>
-        </div>
+        <main className="flex flex-col h-full items-center pt-16">
+            <div className="flex items-end">
+                <h1 className="font-mono font-bold text-2xl" data-cursor="text">
+                    Loading…
+                </h1>
+                <div
+                    className={`w-3 h-1 mb-1 ml-1 bg-cyan-600 ${!showCursor && "opacity-0"}`}
+                />
+            </div>
+        </main>
     );
 }
