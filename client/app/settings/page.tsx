416425cf0a9066b0aeba99f7b47290e2b7ad271f
vgnz93hs <ilce6400.mail@icloud.com>
2026-05-23 11:55:47 +0900

    feature: create server connection settings

--- client/app/settings/page.tsx
+++ client/app/settings/page.tsx
@@ -1,8 +1,50 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
 export default function Settings() {
+    const [serverUrl, setServerUrl] = useState<string>(() => {
+        if (typeof window !== "undefined") {
+            return localStorage.getItem("server-url") || "";
+        }
+        return "";
+    });
+
+    const handleSave = (url: string) => {
+        setServerUrl(url);
+        localStorage.setItem("server-url", url);
+    };
+
     return (
-        <div>
-            <h1>Settings</h1>
-            <div>Overwrite Cnnection</div>
+        <div className="h-full flex flex-col gap-4 pt-16">
+            <h1 data-cursor="text" className="font-bold">
+                Settings
+            </h1>
+            <div data-cursor="text">Overwrite Connection</div>
+            <div className="flex">
+                <input
+                    placeholder="Server URL"
+                    type="url"
+                    value={serverUrl}
+                    data-cursor="text"
+                    onChange={(e) => {
+                        setServerUrl(e.target.value);
+                        handleSave(e.target.value);
+                    }}
+                    className="outline-1"
+                />
+                <button
+                    className="bg-cyan-600"
+                    data-cursor="button"
+                    data-cursor-shape="2"
+                    onClick={() => {
+                        setServerUrl("");
+                        handleSave("");
+                    }}
+                >
+                    Use Default
+                </button>
+            </div>
         </div>
     );
 }
