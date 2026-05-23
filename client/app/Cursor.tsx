9b41cd2ffbcf06328c4afa413d200f25068bc8b4
vgnz93hs <142011868+vgnz93hs@users.noreply.github.com>
2026-05-23 12:05:35 +0900

    fix: fix issue when page moved (#3)

--- client/app/Cursor.tsx
+++ client/app/Cursor.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, useCallback } from "react";
 import { motion, useSpring, useTransform } from "framer-motion";
+import { usePathname } from "next/navigation";
 
 type Button = {
     x: number;
@@ -21,8 +22,15 @@ type Text = {
 };
 
 export function Cursor() {
+    const pathname = usePathname();
+
     const buttons = useRef<Button[]>([]);
     const texts = useRef<Text[]>([]);
+
+    const mouse = useRef({
+        x: 0,
+        y: 0,
+    });
 
     const target = useRef({
         x: 0,
@@ -33,7 +41,8 @@ export function Cursor() {
         opacity: 0,
         borderRadius: 12.5,
     });
-    const [isMouseDown, setIsMouseDown] = useState<boolean>(false);
+
+    const [isMouseDown, setIsMouseDown] = useState(false);
     const isMouseDownRef = useRef(false);
 
     const springConfig = {
@@ -44,10 +53,14 @@ export function Cursor() {
 
     const cursorX = useSpring(0, springConfig);
     const cursorY = useSpring(0, springConfig);
+
     const cursorW = useSpring(25, springConfig);
     const cursorH = useSpring(25, springConfig);
+
     const cursorWeight = useSpring(3, springConfig);
+
     const cursorOpacity = useSpring(0, springConfig);
+
     const cursorBorderRadius = useSpring(12.5, springConfig);
 
     const boxShadow = useTransform(
@@ -56,99 +69,72 @@ export function Cursor() {
             `0 0 0 ${latestWeight}px inset var(--color-foreground)`,
     );
 
-    useEffect(() => {
-        const getElements = () => {
-            const buttonElements = document.querySelectorAll(
-                '[data-cursor="button"]',
-            );
-            const textElements = document.querySelectorAll(
-                '[data-cursor="text"]',
-            );
+    const getElements = useCallback(() => {
+        const buttonElements = document.querySelectorAll(
+            '[data-cursor="button"]',
+        );
 
-            buttons.current = Array.from(buttonElements).map((element) => {
-                const htmlElement = element as HTMLElement;
+        const textElements = document.querySelectorAll('[data-cursor="text"]');
 
-                const rect = htmlElement.getBoundingClientRect();
+        buttons.current = Array.from(buttonElements).map((element) => {
+            const htmlElement = element as HTMLElement;
 
-                const style = window.getComputedStyle(htmlElement);
+            const rect = htmlElement.getBoundingClientRect();
 
-                return {
-                    x: rect.left,
-                    y: rect.top,
+            const style = window.getComputedStyle(htmlElement);
 
-                    width: rect.width,
-                    height: rect.height,
+            return {
+                x: rect.left,
+                y: rect.top,
 
-                    borderRadius: parseFloat(style.borderRadius),
+                width: rect.width,
+                height: rect.height,
 
-                    shape: Number(htmlElement.dataset.cursorShape ?? 0),
-                };
-            });
+                borderRadius: parseFloat(style.borderRadius),
 
-            texts.current = Array.from(textElements).map((element) => {
-                const htmlElement = element as HTMLElement;
-
-                const rect = htmlElement.getBoundingClientRect();
-
-                const style = window.getComputedStyle(htmlElement);
-
-                return {
-                    x: rect.left,
-                    y: rect.top,
-
-                    width: rect.width,
-                    height: rect.height,
-
-                    fontSize: parseFloat(style.fontSize),
-                };
-            });
-        };
-
-        window.addEventListener("mousedown", () => {
-            setIsMouseDown(true);
-            isMouseDownRef.current = true;
+                shape: Number(htmlElement.dataset.cursorShape ?? 0),
+            };
         });
 
-        window.addEventListener("mouseup", () => {
-            setIsMouseDown(false);
-            isMouseDownRef.current = false;
+        texts.current = Array.from(textElements).map((element) => {
+            const htmlElement = element as HTMLElement;
+
+            const rect = htmlElement.getBoundingClientRect();
+
+            const style = window.getComputedStyle(htmlElement);
+
+            return {
+                x: rect.left,
+                y: rect.top,
+
+                width: rect.width,
+                height: rect.height,
+
+                fontSize: parseFloat(style.fontSize),
+            };
         });
-
-        getElements();
-
-        window.addEventListener("resize", getElements);
-        window.addEventListener("scroll", getElements);
-        window.addEventListener("mousedown", getElements);
-
-        return () => {
-            window.removeEventListener("resize", getElements);
-            window.removeEventListener("scroll", getElements);
-            window.removeEventListener("mousedown", getElements);
-        };
     }, []);
 
-    useEffect(() => {
-        const handleMouseMove = (e: MouseEvent) => {
-            const mouseX = e.clientX;
-            const mouseY = e.clientY;
+    const handleMouseMove = useCallback(
+        (x: number, y: number) => {
+            mouse.current.x = x;
+            mouse.current.y = y;
 
-            const selectedButtons: Button[] = buttons.current.filter(
-                (element) => {
-                    return (
-                        mouseX >= element.x &&
-                        mouseX <= element.x + element.width &&
-                        mouseY >= element.y &&
-                        mouseY <= element.y + element.height
-                    );
-                },
-            );
-
-            const selectedTexts: Text[] = texts.current.filter((element) => {
+            const selectedButtons = buttons.current.filter((element) => {
                 return (
-                    mouseX >= element.x &&
-                    mouseX <= element.x + element.width &&
-                    mouseY >= element.y &&
-                    mouseY <= element.y + element.height
+                    x >= element.x &&
+                    x <= element.x + element.width &&
+                    y >= element.y &&
+                    y <= element.y + element.height
+                );
+            });
+
+            const selectedTexts = texts.current.filter((element) => {
+                return (
+                    x >= element.x &&
+                    x <= element.x + element.width &&
+                    y >= element.y &&
+                    y <= element.y + element.height
                 );
             });
 
@@ -156,60 +142,80 @@ export function Cursor() {
                 if (selectedButtons[0].shape == 0) {
                     target.current = {
                         x: selectedButtons[0].x + selectedButtons[0].width / 2,
+
                         y: selectedButtons[0].y + selectedButtons[0].height / 2,
+
                         width: selectedButtons[0].width + 16,
                         height: selectedButtons[0].height + 16,
+
                         weight: 4,
                         opacity: 0.5,
+
                         borderRadius: selectedButtons[0].borderRadius + 8,
                     };
                 } else if (selectedButtons[0].shape == 1) {
                     target.current = {
                         x: selectedButtons[0].x + selectedButtons[0].width / 2,
+
                         y: selectedButtons[0].y + selectedButtons[0].height / 2,
+
                         width: selectedButtons[0].width,
                         height: selectedButtons[0].height,
+
                         weight: 1000,
                         opacity: 0.25,
+
                         borderRadius: selectedButtons[0].borderRadius,
                     };
                 } else if (selectedButtons[0].shape == 2) {
                     target.current = {
-                        x: e.clientX,
-                        y: e.clientY,
+                        x,
+                        y,
+
                         width: 25,
                         height: 25,
+
                         weight: isMouseDownRef.current ? 8 : 4,
+
                         opacity: 1,
                         borderRadius: 12.5,
                     };
                 }
             } else if (selectedTexts.length > 0) {
                 target.current = {
-                    x: e.clientX,
-                    y: e.clientY,
+                    x,
+                    y,
+
                     width: 3,
                     height: selectedTexts[0].fontSize + 10,
+
                     weight: 2,
                     opacity: 0.5,
+
                     borderRadius: 12.5,
                 };
             } else {
                 target.current = {
-                    x: e.clientX,
-                    y: e.clientY,
+                    x,
+                    y,
+
                     width: 20,
                     height: 20,
+
                     weight: 10,
                     opacity: 1,
+
                     borderRadius: 10,
                 };
             }
 
             cursorX.set(target.current.x - target.current.width / 2);
+
             cursorY.set(target.current.y - target.current.height / 2);
+
             cursorW.set(target.current.width);
             cursorH.set(target.current.height);
+
             cursorOpacity.set(target.current.opacity);
 
             const minimumWeight = Math.min(
@@ -222,39 +228,95 @@ export function Cursor() {
             cursorBorderRadius.set(
                 Math.min(target.current.borderRadius, minimumWeight),
             );
+        },
+        [
+            cursorX,
+            cursorY,
+            cursorW,
+            cursorH,
+            cursorOpacity,
+            cursorWeight,
+            cursorBorderRadius,
+        ],
+    );
+
+    useEffect(() => {
+        const onMouseMove = (e: MouseEvent) => {
+            handleMouseMove(e.clientX, e.clientY);
         };
 
-        window.addEventListener("mousemove", handleMouseMove);
+        const onMouseDown = () => {
+            setIsMouseDown(true);
+            isMouseDownRef.current = true;
 
-        window.addEventListener("mousedown", handleMouseMove);
+            handleMouseMove(mouse.current.x, mouse.current.y);
+        };
 
-        window.addEventListener("volumechange", () => handleMouseMove);
+        const onMouseUp = () => {
+            setIsMouseDown(false);
+            isMouseDownRef.current = false;
 
-        window.addEventListener("mouseup", handleMouseMove);
+            handleMouseMove(mouse.current.x, mouse.current.y);
+        };
+
+        getElements();
+
+        window.addEventListener("mousemove", onMouseMove);
+
+        window.addEventListener("mousedown", onMouseDown);
+
+        window.addEventListener("mouseup", onMouseUp);
+
+        window.addEventListener("resize", getElements);
+
+        window.addEventListener("scroll", getElements);
 
         return () => {
-            window.removeEventListener("mousemove", handleMouseMove);
+            window.removeEventListener("mousemove", onMouseMove);
+
+            window.removeEventListener("mousedown", onMouseDown);
+
+            window.removeEventListener("mouseup", onMouseUp);
+
+            window.removeEventListener("resize", getElements);
+
+            window.removeEventListener("scroll", getElements);
         };
-    });
+    }, [getElements, handleMouseMove]);
+
+    useEffect(() => {
+        requestAnimationFrame(() => {
+            getElements();
+
+            handleMouseMove(mouse.current.x, mouse.current.y);
+        });
+    }, [pathname, getElements, handleMouseMove]);
 
     return (
         <motion.div
             className={`
                 ${isMouseDown && "scale-95"}
+
                 transition-transform
                 duration-200
                 ease-out
+
                 pointer-events-none
+
                 fixed
                 rounded-full
             `}
             style={{
                 opacity: cursorOpacity,
+
                 left: cursorX,
                 top: cursorY,
+
                 width: cursorW,
                 height: cursorH,
+
                 boxShadow,
+
                 borderRadius: cursorBorderRadius,
             }}
         />
