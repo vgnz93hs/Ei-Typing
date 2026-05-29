b4a86196eca64e456a8a6a83c5045dd4827be7e4
vgnz93hs <ilce6400.mail@icloud.com>
2026-05-29 22:14:37 +0900

    feature: keyboard client

--- client/components/feature/InputView.tsx
+++ client/components/feature/InputView.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function TypingView({
     japanese,
@@ -11,7 +11,15 @@ export default function TypingView({
         english ? Array(english.length).fill("") : [],
     );
 
-    const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
+    const [currentSelection, setCurrentSelection] = useState(0);
+
+    const inputRef = useRef<HTMLInputElement | null>(null);
+
+    const [charInput, setCharInput] = useState("");
+
+    useEffect(() => {
+        inputRef.current?.focus();
+    }, []);
 
     return (
         <div className="flex flex-col gap-2">
@@ -21,16 +29,142 @@ export default function TypingView({
                 </div>
             </div>
 
-            <div className="w-full flex justify-center">
-                <div className="w-fit gap-2 flex justify-start">
-                    {input.map((char, index) => (
-                        <div
-                            key={index}
-                            className="font-bold text-center w-8 h-12 text-2xl border border-(--color-border) rounded-lg caret-transparent"
+            {english && (
+                <div className="w-full flex justify-center">
+                    <div
+                        className="w-fit
+                        rounded-lg border border-(--color-border) overflow-clip gap-y-3 flex-wrap flex justify-start"
+                    >
+                        {[...english].map((char, index) =>
+                            char === " " ? (
+                                <div className="w-4 h-10" key={index} />
+                            ) : (
+                                <button
+                                    key={index}
+                                    className={`
+                                        font-bold
+                                        flex
+                                        items-center
+                                        justify-center
+                                        w-6
+                                        h-10
+                                        text-xl
+                                        transition-all
+                                        duration-150
+                                        ease-out
+                                        border-(--color-border)
+
+                                        ${
+                                            index === currentSelection &&
+                                            "bg-(--color-border)"
+                                        }
+                                    `}
+                                    data-cursor="text"
+                                    onClick={() => {
+                                        setCurrentSelection(index);
+
+                                        requestAnimationFrame(() => {
+                                            inputRef.current?.focus();
+                                        });
+                                    }}
+                                >
+                                    {input[index]}
+                                </button>
+                            ),
+                        )}
+
+                        <input
+                            ref={inputRef}
+                            value={charInput}
+                            onChange={(e) => {
+                                const value = e.target.value;
+
+                                if (!value) return;
+
+                                const char = value.slice(-1);
+
+                                const next = [...input];
+                                next[currentSelection] = char;
+
+                                setInput(next);
+
+                                setCharInput("");
+
+                                let nextIndex = currentSelection + 1;
+
+                                while (english[nextIndex] === " ") {
+                                    nextIndex++;
+                                }
+
+                                if (nextIndex < english.length) {
+                                    setCurrentSelection(nextIndex);
+                                }
+                            }}
+                            onKeyDown={(e) => {
+                                // ←
+                                if (e.key === "ArrowLeft") {
+                                    e.preventDefault();
+
+                                    let next = currentSelection - 1;
+
+                                    while (english[next] === " ") {
+                                        next--;
+                                    }
+
+                                    setCurrentSelection(Math.max(0, next));
+                                }
+
+                                if (e.key === "ArrowRight") {
+                                    e.preventDefault();
+
+                                    let next = currentSelection + 1;
+
+                                    while (english[next] === " ") {
+                                        next++;
+                                    }
+
+                                    setCurrentSelection(
+                                        Math.min(english.length - 1, next),
+                                    );
+                                }
+
+                                if (e.key === "Backspace") {
+                                    e.preventDefault();
+
+                                    const next = [...input];
+
+                                    if (next[currentSelection]) {
+                                        next[currentSelection] = "";
+                                        setInput(next);
+                                        return;
+                                    }
+
+                                    let prev = currentSelection - 1;
+
+                                    while (english[prev] === " ") {
+                                        prev--;
+                                    }
+
+                                    if (prev >= 0) {
+                                        next[prev] = "";
+
+                                        setInput(next);
+                                        setCurrentSelection(prev);
+                                    }
+                                }
+                            }}
+                            className="
+                                absolute
+                                opacity-0
+                                pointer-events-none
+                            "
+                            autoCapitalize="off"
+                            autoCorrect="off"
+                            spellCheck={false}
                         />
-                    ))}
+                    </div>
                 </div>
-            </div>
+            )}
         </div>
     );
 }
